843d92d8679c208c74a2a5e55767c3f777eddffa
Kelvin Bush <kelybush@gmail.com>
2022-07-14 16:45:24 +0300

    fix linter errors

--- lib/my/my_list.rb
+++ lib/my/my_list.rb
@@ -1,13 +1,1 @@
-require_relative './my_enumerable'
-
-class MyList
-  include MyEnumerable
-
-  def initialize(*list)
-    @list = list
-  end
-
-  def each(&block)
-    @list.each(&block)
-  end
-end
+require_relative './my_enumerable'class MyList  include MyEnumerable  def initialize(*list)    @list = list  end  def each(&block)    @list.each(&block)  endend
